fbef0122ef7e3ba429798a033e1842814805f4cf
tsmame767 <tsmane789@gmail.com>
2023-12-22 16:09:00 +0530

    updated uc1

--- uc1.py
+++ uc1.py
@@ -1,7 +1,10 @@
 #check whether the employee is present or not
-user=int(input("enter 1 for present 0 for absent: "))
-if user==1:
-    print("present")
+def presence():
+    user=int(input("enter 1 for present 0 for absent: "))
+    if user==1:
+        return "present"
 
-else:
-    print("absent")
+    else:
+        return "absent"
+    
+presence()
